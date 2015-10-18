a4791c176b0e0ac1515c1d40f681541f7f81335c
bibio <babiy3104@gmail.com>
2015-10-18 03:43:28 +0000

    追加

---
+++ cookbooks/dbserver/recipes/default.rb
@@ -0,0 +1,22 @@
+#
+# Cookbook Name:: dbserver
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+
+
+remote_file '/tmp/mysql-community-release-el6-5.noarch.rpm' do
+  source 'https://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm'
+end
+
+package 'mysql-community-release-el6-5.noarch.rpm' do
+  source '/tmp/mysql-community-release-el6-5.noarch.rpm'
+  provider Chef::Provider::Package::Rpm
+end
+
+package 'mysql-community-server'
+package 'mysql-community-devel'
+
+service 'mysqld' do
+  action [:enable, :start]
+end
