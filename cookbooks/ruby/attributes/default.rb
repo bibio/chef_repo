60a60099069db20c4bddc47c111f78d20878db3a
bibio <babiy3104@gmail.com>
2015-10-18 06:00:55 +0000

    add ruby build

---
+++ cookbooks/ruby/attributes/default.rb
@@ -0,0 +1,5 @@
+default['ruby']['user'] = 'vagrant'
+default['ruby']['rbenv_git_repository'] = 'https://github.com/sstephenson/rbenv.git'
+default['ruby']['ruby_build_git_repository'] = 'https://github.com/sstephenson/ruby-build.git'
+default['ruby']['ruby_version'] = '2.2.3'
+
